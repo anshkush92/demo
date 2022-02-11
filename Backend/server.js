bb5cbf20ab5ba644ab38d6dec240a4a843e1b78f
Ansh <imt_2020013@iiitm.ac.in>
2022-02-12 01:20:47 +0530

    Added the ability to store data entered in form in JSON files

--- Backend/server.js
+++ Backend/server.js
@@ -16,11 +16,23 @@ const app = express();
 app.use(express.static("public"));
 
 // Theory: For checking that is there any formData in the response or not (if there is it decoded that data by using this) 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // Theory: Telling the template engine which folder file will work as templates || changing the template engine of the Express
 app.set("views");
 app.set("view engine", "ejs");
+
+// Test ---------------------------------------------------------- Functions for code reuseability ---------------------------------------
+
+// Theory: Function which is use to read-write data in the JSON files used for data collected from contact-us and newsletter
+function read_write_data_in_json_files(file_name, data_entered) {
+    let file_path = path.join(__dirname, "data", file_name);
+    let data_in_file_raw_format = fs.readFileSync(file_path);
+    let data_in_file_json_format = JSON.parse(data_in_file_raw_format);
+
+    data_in_file_json_format.push(data_entered);
+    fs.writeFileSync(file_path, JSON.stringify(data_in_file_json_format));
+}
 
 // Test ---------------------------------------------------------- Routes ----------------------------------------------------------------
 
@@ -49,8 +61,23 @@ app.get("/contact", function (req, res) {
     res.render("contact-us-form");
 })
 
+// Theory: POST request in case of form submission
+app.post("/contact", function (req, res) {
+    read_write_data_in_json_files("contact-us.json", req.body);
+
+    res.redirect("/");
+});
+
+// Debug ---------------------------------------------------------- URL --> localhost:3000/newsletter ---------------------------------------
+app.post("/newsletter", function (req, res) {
+    read_write_data_in_json_files("newsletter.json", req.body);
+
+    res.redirect("/");
+});
+
 // Test ---------------------------------------------------------- Server starting -------------------------------------------------------
 app.listen(3000, function (req, res) {
     console.log("Server is listening on the port 3000");
     console.log("Access the server by typing localhost:3000")
 })
+
