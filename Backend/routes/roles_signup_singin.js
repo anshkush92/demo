591905396600500e8e60e1f2e00e8e7f0bb544cc
Ansh <imt_2020013@iiitm.ac.in>
2022-02-19 21:20:10 +0530

    Added the admin login and admin upload page | Added /admin-login and /admin-upload routes

--- Backend/routes/roles_signup_singin.js
+++ Backend/routes/roles_signup_singin.js
@@ -6,5 +6,18 @@ router.get("/roles", function (req, res) {
     res.render("roles");
 })
 
+// Debug ---------------------------------------------------------- URL --> localhost:3000/admin-login ---------------------------------
+
+// Todo: To check whether the admin credentials are correct or not 
+router.get("/admin-login", function (req, res) {
+    res.render("admin");
+});
+
+// Debug ---------------------------------------------------------- URL --> localhost:3000/admin-login ---------------------------------
+
+router.get("/admin-upload", function (req, res) {
+    res.render("admin-upload");
+})
+
 // Test ------------------------------------------------------------ Exporting this file code -----------------------------------------
 module.exports = router;
